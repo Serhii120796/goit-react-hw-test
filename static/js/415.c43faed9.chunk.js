8f1d861052b13614a46fdb94fa8ae2a2dca03dd9
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2023-11-20 19:03:13 +0000

    Deploying to gh-pages from @ Serhii120796/goit-react-hw-test@37262a19daadc8d3d41c0181b294c41b6caa505e 🚀

---
+++ static/js/415.c43faed9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r),t.d(r,{default:function(){return a}});var n=t(184);function a(){return(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Task manager welcome page"})})}}}]);
+//# sourceMappingURL=415.c43faed9.chunk.js.map
